708914aa48e5709eebc643031a58222b726f6978
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-04 10:09:22 +0000

    Add parsing prototype function

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,17 @@
 #include "parser.h"
+#include <stdio.h>
+#include <string.h>
+
+int parse_url(url_parser *parser, char *url)
+{
+        char *head;
+        char *cp;
+        cp = strdup(url);
+        if (cp == NULL) {
+                fprintf(stderr, "Error copying url");
+                return -1;
+        }
+        head = strtok(cp, "/:@");
+        printf("The head will be: %s", head);
+        return 0;
+}
--- src/parser.h
+++ src/parser.h
@@ -1,4 +1,23 @@
 #ifndef RCOM_PARSER_H_
 #define RCOM_PARSER_H_
 
+/**
+ * A url should have the following syntax:
+ *
+ * ftp://[<user>:<password>@]<host>/<path>
+ *
+ * ftp:// -> obligatory
+ * [<user>:<password>] -> optional
+ * host -> obligatory
+ * path -> obligatory
+ *
+ **/
+
+typedef struct {
+        char *user;
+        char *password;
+        char *host;
+        char *path;
+} url_parser;
+
 #endif // RCOM_PARSER_H_
