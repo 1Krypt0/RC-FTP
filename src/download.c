34956e814a7f16c0ef932396df017be68c02b5b1
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-05 19:45:51 +0000

    Reorganizes code into files

--- src/download.c
+++ src/download.c
@@ -1,91 +1,20 @@
 #include "parser.h"
-#include "connection.h"
-#include "communication.h"
-#include "defines.h"
+#include "file.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-        int socket_fd = -1;
         struct url_parser url;
         if (parse_url(&url, argv[1])) {
                 fprintf(stderr, "Error parsing url\n");
                 return EXIT_FAILURE;
         }
-        socket_fd = start_connection(url.ip, DEFAULT_FTP_PORT);
-        if (socket_fd == EXIT_FAILURE) {
-                fprintf(stderr, "Error starting connection\n");
+
+        if (transfer_file(&url)) {
+                fprintf(stderr, "Error transfering file\n");
                 return EXIT_FAILURE;
         }
-
-        struct server_response response;
-        memset(&response, 0, sizeof(struct server_response));
-
-        read_response(socket_fd, &response);
-
-        if (login(socket_fd, url.user, url.password)) {
-                fprintf(stderr, "Error logging in\n");
-                return EXIT_FAILURE;
-        }
-
-        printf("Succesfully logged in!\n");
-        memset(&response, 0, sizeof(struct server_response));
-
-        if (enter_passive_mode(socket_fd, &response)) {
-                fprintf(stderr, "Error entering passive mode\n");
-                return EXIT_FAILURE;
-        }
-
-        char *ip = malloc(16);
-        int *port = malloc(sizeof(int));
-        if (convert_to_port(response.response, ip, port)) {
-                fprintf(stderr,
-                        "Error converting bytes received in response\n");
-                free(ip);
-                free(port);
-                return EXIT_FAILURE;
-        }
-
-        int data_fd = -1;
-        if ((data_fd = start_connection(ip, *port)) == EXIT_FAILURE) {
-                fprintf(stderr, "Error connecting to other port\n");
-                free(ip);
-                free(port);
-                return EXIT_FAILURE;
-        }
-
-        free(ip);
-        free(port);
-
-        if (request_file(socket_fd, url.path)) {
-                fprintf(stderr, "Error retrieving file\n");
-                return EXIT_FAILURE;
-        }
-
-        memset(&response, 0, sizeof(struct server_response));
-        if (read_response(socket_fd, &response)) {
-                fprintf(stderr, "Error reading response\n");
-                return EXIT_FAILURE;
-        }
-
-        if (response.response_code != TRANSFER_COMPLETE) {
-                fprintf(stderr, "File transfer not complete\n");
-                return EXIT_FAILURE;
-        }
-
-        // transfer file goes here
-
-        if (logout(socket_fd)) {
-                fprintf(stderr, "Error quitting\n");
-                return EXIT_FAILURE;
-        }
-
-        printf("Succesfully requested file %s from server\n", url.path);
-
-        close(socket_fd);
 
         return EXIT_SUCCESS;
 }
--- src/file.h
+++ src/file.h
@@ -1,4 +1,9 @@
 #ifndef RCOM_FILE_H_
 #define RCOM_FILE_H_
 
+#include "parser.h"
+int read_file(int socket_fd, char *file_name);
+
+int transfer_file(struct url_parser *url);
+
 #endif // RCOM_FILE_H_
