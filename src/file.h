824acf2123ca3875d3400c353fc1103413315812
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-10 09:27:46 +0000

    Finish documentation on file module

--- src/file.h
+++ src/file.h
@@ -3,8 +3,26 @@
 
 #include "parser.h"
 
+/**
+ * @brief Reads the file from the open socket and writes into a file with the
+ * same name
+ *
+ * @param socket_fd File descriptor of the open socket
+ *
+ * @param file_path Absolute path to the file
+ *
+ * @return 0 if successful , 1 otherwise
+ */
 int read_file(int socket_fd, char *file_path);
 
+/**
+ * @brief Transfers the file from the remove server on to the local machine
+ *
+ * @param url Struct holding the necessary parameters to determine the host,
+ * the IP address, the file path and the user and password, if necessary
+ *
+ * @return 0 if successful, 1 otherwise
+ * */
 int transfer_file(struct url_parser *url);
 
 #endif // RCOM_FILE_H_
